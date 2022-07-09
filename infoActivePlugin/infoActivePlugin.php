1271e37429f44b75660d60938bd5740a43e4b92a
gcyrillus <1832331+gcyrillus@users.noreply.github.com>
2022-07-10 00:40:30 +0200

    Add files via upload

---
+++ infoActivePlugin/infoActivePlugin.php
@@ -0,0 +1,27 @@
+<?php
+class infoActivePlugin extends plxPlugin {	 
+
+	public function __construct($default_lang) {
+
+		# appel du constructeur de la classe plxPlugin (obligatoire)
+		parent::__construct($default_lang);
+		
+		# déclaration des hooks
+		$this->addHook('AdminSettingsInfos', 'AdminSettingsInfos');	
+	}
+
+	/* affiche les plugins actis pour Infos. */
+	public function AdminSettingsInfos() {
+	echo '<?php ';
+		?>
+		echo '<table><thead><tr><th>'.L_PLUGINS_ACTIVE_LIST.' </th></tr></thead><tbody>';
+		foreach($plxAdmin->plxPlugins->aPlugins as $key => $value) {
+			echo '<tr><td>'.$key.'</td></tr>';
+		}
+		echo '</tbody></table>';
+	?>
+		<?php 
+	
+	}
+}
+?>	
